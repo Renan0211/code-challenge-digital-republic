b6fb274c800546a642105f962ec82425c5b349fb
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-05 18:39:06 -0300

    cria uma funçao para calcular a area de cada parede e outra para calcular a soma de todas as areas e chama a segunda na funcao que retornara as latas necessarias

--- models/paintingModel.js
+++ models/paintingModel.js
@@ -4,9 +4,27 @@ const minDifBetweenDoorAndCeiling = 0.3;
 const areaPerLiter = 5;
 const cans = [0.5, 2.5, 3.6, 18];
 
-const validateWallMesure = (wallMesures) => {
-  const { width, height, includesDoor } = wallMesures;
+const validateWallMesure = (wallMeasures, includesDoor) => {
+  const { width, height } = wallMeasures;
   if (width < 1 || width > 15) return false;
   if (includesDoor && height - doorMeasures.height < minDifBetweenDoorAndCeiling) return false;
   return true;
+};
+
+const calculateWallArea = (wallMeasures) => {
+  const { width, height } = wallMeasures;
+  return width * height;
+};
+
+const calculateTotalWallArea = (walls) => {
+  let totalWallArea = 0;
+  walls.forEach((wall) => {
+    const wallArea = calculateWallArea(wall.wallMeasures);
+    totalWallArea += wallArea;
+  });
+  return totalWallArea;
+};
+
+const returnNecessaryCans = (walls) => {
+  const totalWallArea = calculateTotalWallArea(walls);
 };
