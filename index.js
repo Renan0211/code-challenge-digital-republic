522a93afa246d42520d796c43719ab7b3a02b6ce
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-10 11:21:53 -0300

    cria o index, a aplicacao ira escutar na porta 3000 e recebera a requisiçao em /cans para retornar as latas necessarias

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const paintingController = require('./controllers/paintingController');
+
+const app = express();
+
+app.get('/cans', paintingController.getNecessaryCans);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
